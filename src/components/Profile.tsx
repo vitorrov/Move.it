17fd26bc093e270448921d372699346e7dba28ed
Vitor <vitormr45@gmail.com>
2021-02-28 18:03:36 -0300

    updates

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,7 +7,10 @@ export function Profile() {
 
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/vitorrov.png" alt="Vitor" />
+      <img
+        src="https://image.freepik.com/vetores-gratis/perfil-de-avatar-de-homem-no-icone-redondo_24640-14044.jpg"
+        alt="Vitor"
+      />
       <div>
         <strong>Vitor</strong>
         <p>
